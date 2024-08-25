1aaf4642382b27af47a16326cec723a1eb9d7b21
prakashrajavel <prakashprithiv0@gmail.com>
2024-08-25 21:43:51 +0530

    c1ssw

--- cal.java
+++ cal.java
@@ -1,6 +1,10 @@
 public class cal {
     public int sum(int a, int b)
     {
-        return a+b;
+        return a+b+1;
+    }
+    public int mul(int a, int b)
+    {
+        return a*b*1;
     }
 }
